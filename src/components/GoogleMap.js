445925644b77cf129ef69a17f196def96a6d32fa
flamebeard <ethan@hursty.co.nz>
2021-07-08 15:42:27 +1000

    updated map location

--- src/components/GoogleMap.js
+++ src/components/GoogleMap.js
@@ -10,8 +10,8 @@ if (process.env.GATSBY_NETLIFY_MAP_KEY) {
 class GoogleMap extends Component {
   static defaultProps = {
     center: {
-      lat: -28.0914483,
-      lng: 153.4425208
+      lat: -27.468325219812517,
+      lng: 153.02690854524323
     },
     zoom: 14
   }
@@ -25,7 +25,7 @@ class GoogleMap extends Component {
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
         >
-          <Marker lat={-28.0914483} lng={153.4425208} text={'Kreyser Avrora'} />
+          <Marker lat={-27.468325219812517} lng={153.02690854524323} text={'Kreyser Avrora'} />
         </GoogleMapReact>
       </div>
     )
