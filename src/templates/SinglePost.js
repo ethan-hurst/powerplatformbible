20dbef8877ce3bf8910db2b9a0495e29fce2d4aa
flamebeard <ethan@hursty.co.nz>
2021-07-09 09:26:17 +1000

    reverting

--- src/templates/SinglePost.js
+++ src/templates/SinglePost.js
@@ -122,7 +122,7 @@ export const pageQuery = graphql`
         title
         template
         subtitle
-        date(formatString: "MMMM DD, YYYY")
+        date(formatString: "MMMM Do, YYYY")
         categories {
           category
         }
