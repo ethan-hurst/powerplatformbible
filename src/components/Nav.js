24747302f2d2e495a970e195403590db12f9994b
Ethan Hurst <ethan.hurst@clade.com.au>
2021-07-15 10:29:46 +1000

    more routing changes

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -73,42 +73,6 @@ export class Navigation extends Component {
           <div className="Nav--Links">
             <NavLink to="/">Home</NavLink>
             <NavLink to="/guides/">Guides</NavLink>
-            {/* <div
-              className={`Nav--Group ${
-                this.state.activeSubNav === 'posts' ? 'active' : ''
-              }`}
-            >
-              <span
-                className={`NavLink Nav--GroupParent ${
-                  this.props.location.pathname.includes('posts') ||
-                  this.props.location.pathname.includes('blog') ||
-                  this.props.location.pathname.includes('post-categories')
-                    ? 'active'
-                    : ''
-                }`}
-                onClick={() => this.toggleSubNav('posts')}
-                onKeyDown={e => this.keyToggleSubNav(e, 'posts')}
-                tabIndex={0}
-                aria-label="Navigation"
-                role="button"
-              >
-                Guides
-                <div className="Nav--GroupLinks">
-                  <NavLink to="/blog/" className="Nav--GroupLink">
-                    All Posts
-                  </NavLink>
-                  {subNav.posts.map((link, index) => (
-                    <NavLink
-                      to={link.slug}
-                      key={'posts-subnav-link-' + index}
-                      className="Nav--GroupLink"
-                    >
-                      {link.title}
-                    </NavLink>
-                  ))}
-                </div>
-              </span>
-            </div> */}
             <NavLink to="/contact/">Contact</NavLink>
           </div>
           <button
