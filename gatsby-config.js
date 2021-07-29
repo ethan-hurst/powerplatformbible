575fd3db081f6b70e90054c942790771f4b5a925
Ethan Hurst <Ethan.Hurst@clade.com.au>
2021-07-29 12:49:25 +1000

    added gtm plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,9 +9,9 @@ module.exports = {
     'gatsby-plugin-react-helmet',
     'gatsby-transformer-yaml',
     {
-      resolve: "gatsby-plugin-google-tagmanager",
+      resolve: 'gatsby-plugin-google-tagmanager',
       options: {
-        id: "GTM-5S7ZM97",
+        id: 'GTM-5S7ZM97',
 
         // Include GTM in development.
         //
@@ -22,18 +22,18 @@ module.exports = {
         // should be an object or a function that is executed in the browser
         //
         // Defaults to null
-        defaultDataLayer: { platform: "gatsby" },
+        defaultDataLayer: { platform: 'gatsby' },
 
         // Specify optional GTM environment details.
-        gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
-        gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
-        dataLayerName: "YOUR_DATA_LAYER_NAME",
+        gtmAuth: 'YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING',
+        gtmPreview: 'YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME',
+        dataLayerName: 'YOUR_DATA_LAYER_NAME',
 
         // Name of the event that is triggered
         // on every Gatsby route change.
         //
         // Defaults to gatsby-route-change
-        routeChangeEventName: "powerplatformevent",
+        routeChangeEventName: 'powerplatformevent',
         // Defaults to false
         enableWebVitalsTracking: true,
       },
